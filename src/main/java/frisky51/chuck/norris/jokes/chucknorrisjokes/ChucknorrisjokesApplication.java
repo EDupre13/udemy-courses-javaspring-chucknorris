eed94c1d1c736ed051162dcc8078e76a64f61249
EDupre13 <EDupre13@gmail.com>
2017-10-29 10:33:24 -0500

    refactored as xml config

--- src/main/java/frisky51/chuck/norris/jokes/chucknorrisjokes/ChucknorrisjokesApplication.java
+++ src/main/java/frisky51/chuck/norris/jokes/chucknorrisjokes/ChucknorrisjokesApplication.java
@@ -2,8 +2,10 @@ package frisky51.chuck.norris.jokes.chucknorrisjokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class ChucknorrisjokesApplication {
 
 	public static void main(String[] args) {
